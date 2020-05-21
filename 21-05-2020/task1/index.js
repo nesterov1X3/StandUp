494375d7d1c8e767c241bbc75b896ecbf683e7fe
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-05-21 11:33:41 +0300

    test

---
+++ 21-05-2020/task1/index.js
@@ -0,0 +1,36 @@
+const user = {
+  _firstName: 'John',
+  lastName: 'Doe',
+  getfullName() {
+    return `${this.firstName} ${this.lastName}`
+  },
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`
+  },  
+  setfullName(fullName){
+    [this.firstName, this.lastName] =  fullName.split(' ')
+    // this.firstName = parts[0];
+    // this.lastName = parts[1]
+  },
+  set fullName(fullName){
+    [this.firstName, this.lastName] =  fullName.split(' ')
+    
+  },
+  set firstName(name){ 
+    if(typeof name === 'string'){
+      this._firstName = name
+    }else{
+      console.error('Not a string');
+    }
+  },
+  get firstName(){ 
+    return this._firstName
+  }
+}
+// user.firstName = 1;
+// console.log(user._firstName);
+
+// console.log(user.fullName());
+// user.fullName = 'riki tiki'
+// console.log(user.fullName);
+
