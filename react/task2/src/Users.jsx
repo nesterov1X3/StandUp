3f75acd50987cddc18ae94ccb7d4c14cfea38c46
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-08-19 17:27:33 +0300

    test

--- react/task2/src/Users.jsx
+++ react/task2/src/Users.jsx
@@ -7,29 +7,28 @@ class Users extends Component {
 
 
     render() {
-        const {match}=this.props;
+        const { match } = this.props;
         return (
             <BrowserRouter>
-            <div className="page__content">
-                <h1>Users</h1>
-                <ul className="navigation">
-                    <li className="navigation__item">
-                        <Link to="/users/github">Github</Link>
-                    </li>
-                    <li className="navigation__item">
-                        <Link to="/users/facebook">Facebook</Link>
-                    </li>
-                </ul>
-               
-                    <Route  path={match.path}>
-                    <span>Select user please</span>
-                </Route>
-                <Route path={`${match.path}/:userName`} component={User}></Route>
-                
-                
-               
+                <div className="page__content">
+                    <h1>Users</h1>
+                    <ul className="navigation">
+                        <li className="navigation__item">
+                            <Link to="/users/github">Github</Link>
+                        </li>
+                        <li className="navigation__item">
+                            <Link to="/users/facebook">Facebook</Link>
+                        </li>
+                    </ul>
+                   <Switch>
+                    <Route exact path={match.path}>
+                        <span>Select user please</span>
+                    </Route>
+                    <Route path={`${match.path}/:userName`} component={User}></Route>
+                    </Switch>
 
-            </div>
+
+                </div>
             </BrowserRouter>)
     }
 }
